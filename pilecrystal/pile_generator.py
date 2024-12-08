b4917cd85d3c036f62e077a8c8010ff7ed069dc5
ShotaNamerikawa <shota.namerikawa@gmail.com>
2024-12-09 00:45:14 +0900

    add test

--- pilecrystal/pile_generator.py
+++ pilecrystal/pile_generator.py
@@ -1,19 +1,25 @@
 from pymatgen.core.surface import SlabGenerator
 from pymatgen.core import Structure
-def set_layers(slabgen:SlabGenerator, n_layers_slab, n_layers_vac):
-    """set n_layers of slabgen to user-defined value.
+import click
+
+def set_layers(slabgen:SlabGenerator, n_layers_slab:int, n_layers_vac:int):
+    """set n_layers of SlabGenerator to be user-defined value.
+    
+    set n_layers of an SlabGenerator, which cannot be directly changed in the constructor of the class.
 
     Parameters
     ----------
     slabgen : SlabGenerator
-        _description_
-    n_layers_slab : _type_
-        layer number of slabs
-    n_layers_vac : _type_
-        layer number of vacuum
+        a SlabGenerator instance
+    n_layers_slab : int
+        the layer number of slabs
+    n_layers_vac : int
+        the layer number of vacuum
     """
     h = slabgen._proj_height
     p = round(h / slabgen.parent.lattice.d_hkl(slabgen.miller_index), 8)
+    
+    # set min_slab_size and min_vac_size to be those corresponding_to n_layers_slab and n_layers_vac.
     if slabgen.in_unit_planes == True:
         slabgen.min_slab_size = p*n_layers_slab
         slabgen.min_vac_size = p*n_layers_vac
@@ -21,11 +27,62 @@ def set_layers(slabgen:SlabGenerator, n_layers_slab, n_layers_vac):
         slabgen.min_slab_size = h*n_layers_slab
         slabgen.min_vac_size = h*n_layers_vac
         
-def get_layered_cif(structure:Structure, miller_index, n_layers_slab, n_layers_vac, *slabgen_args, cif_prefix="layered", **slabgen_kwargs):
-    slabgen = SlabGenerator(structure, miller_index, 10,10, *slabgen_args, **slabgen_kwargs)
+def get_layered_cif(structure:Structure, miller_index, n_layers_slab, n_layers_vac, *slabgen_args, 
+                    slab_cif_name="layered.cif", 
+                    max_normal_search: int|None = None, 
+                    **slabgen_kwargs:dict[str,bool]):
+    """get layered cif structure
+    
+    NOTE: If you want miller_index to correspond to a conventional cell, you should pass Structure of a conventional cell!
+
+    Parameters
+    ----------
+    structure : Structure
+        _description_
+    miller_index : _type_
+        _description_
+    n_layers_slab : _type_
+        _description_
+    n_layers_vac : _type_
+        _description_
+    slab_cif_name : str, optional
+        _description_, by default "layered"
+    """
+    min_slab_size = 10
+    min_vacuum_size = 10
+    slabgen = SlabGenerator(structure, miller_index, min_slab_size, min_vacuum_size, *slabgen_args, **slabgen_kwargs)
     set_layers(slabgen, n_layers_slab, n_layers_vac)
     slabs = slabgen.get_slabs()
     cif_str = slabs[0].to(filename=".cif")
-    with open(cif_prefix + ".cif", 'w') as fp:
+    with open(slab_cif_name , 'w') as fp:
         fp.write(cif_str)
         
+@click.command()
+@click.option('--cif', prompt='the name of bulk cif file', help='', required = True) #argument1 of cli function.
+@click.option('--miller', prompt='miller index:',
+              help='For miller index, see the article of wikipedia.',
+              type = int,
+              nargs = 3,
+              required = True
+              )
+@click.option('--num_slab', prompt='the number of slab layers',
+              type = int,
+              default = 5,
+              required = True
+              )
+@click.option('--num_vacuum', prompt='the number of slab layers',
+              type = int,
+              default = 5,
+              required = True
+              )
+@click.option('--slab_cif_name', prompt='the name of the slab cif file created from the original bulk cif file',
+              default = "layered",
+              )
+@click.option('--max_normal_search', 
+              prompt='SlabGenerator.max_normal_search',
+              default = 0,
+              type = int)
+def get_slab(cif:str, miller:tuple[int], num_slab:int, num_vacuum:int, slab_cif_name:str, max_normal_search:int|None = None):
+    bulk_strct = Structure.from_file(cif)
+    get_layered_cif(bulk_strct, miller, num_slab, num_vacuum, slab_cif_name = slab_cif_name, 
+                    max_normal_search=None if max_normal_search == 0 else max_normal_search)
--- tests/test_pile_generator.py
+++ tests/test_pile_generator.py
@@ -1,9 +1,8 @@
-import sys
-sys.path.append("./")
-from pilecrystal.pile_generator import set_layers, get_layered_cif
+from pilecrystal.pile_generator import set_layers, get_layered_cif, get_slab
 from pymatgen.core import Structure
 from pymatgen.core.surface import SlabGenerator
-import os
+from click.testing import CliRunner
+
 def test_set_layers():
     structrue = Structure.from_file("tests/models/Mn2VGa.cif") 
     slabgen = SlabGenerator(structrue,[0,0,1],10,10)
@@ -17,13 +16,22 @@ def test_set_layers():
             anum += 1 
     assert anum == 16
     
-def test_get_cif():
+def test_get_cif(tmp_path):
     cif_file = "tests/models/Mn2VGa.cif"
     structure = Structure.from_file(cif_file)
-    get_layered_cif(structure, [0,0,1], 5, 5, cif_prefix=os.path.join("tests/models","layered"))
+    get_layered_cif(structure, [0,0,1], 5, 5, slab_cif_name= tmp_path)
     
-    
-if __name__ == "__main__":
-    import sys
-    sys.path.append("./")
-    test_set_layers()
+def test_cli(tmp_path):
+    # FIXME: make a bulk file from a Structure instance in this method.
+    runner = CliRunner()
+    result = runner.invoke(get_slab, 
+                           ["tests/models/Mn2VGa.cif", 
+                            (0,0,1),
+                            5,
+                            5,
+                            tmp_path,
+                            None])
+    assert result.exit_code == 0
+    with open(tmp_path) as fp:
+        # add test cif parameter by converting it to Structure.
+        pass
